c7e23c27cfa6e0d777dbc108c0f29f6668c7c7c7
Martbul <martbul01@gmail.com>
2024-03-03 18:37:23 +0200

    nav is done

--- final-project-client/src/app/app.component.spec.ts
+++ final-project-client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'final-project'`, () => {
+  it(`should have as title 'final-project-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('final-project');
+    expect(app.title).toEqual('final-project-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('final-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('final-project-client app is running!');
   });
 });
