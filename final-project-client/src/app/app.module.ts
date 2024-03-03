0ad31375e42f27da88a4944a464453fc955c9710
Martbul <martbul01@gmail.com>
2024-03-04 00:23:58 +0200

    ddd

--- final-project-client/src/app/app.module.ts
+++ final-project-client/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { TestemonialsComponent } from './components/testemonials/testemonials.component';
+import { FAQsComponent } from './components/faqs/faqs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    HomepageComponent,
+    TestemonialsComponent,
+    FAQsComponent
   ],
   imports: [
     BrowserModule,
