8048d330e4808f51aa59d897fa0cee0eb716e505
Martbul <martbul01@gmail.com>
2024-03-12 11:09:33 +0200

    df

--- final-project-client/src/app/types/user.ts
+++ final-project-client/src/app/types/user.ts
@@ -1,16 +1,17 @@
-export interface User{
-    username: string;
-    email: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    appsCreated: string[];
-    appsBought: string[];
-    _id: string;
-    country: string;
-    city:string;
-    phoneNumber:string;
-    aboutMe:string;
-    imgUrl:string
-    __v: number;
+export interface User {
+  username: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  appsCreated: string[];
+  appsBought: string[];
+    appsLiked: string[];
+  _id: string;
+  country: string;
+  city: string;
+  phoneNumber: string;
+  aboutMe: string;
+  imgUrl: string;
+  __v: number;
 }
